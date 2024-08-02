c6593a706a0c0c2d0287db17865b4ac497ef9279
prajwalsiwa <prajwal.siwa1@gmail.com>
2024-08-02 12:51:05 +0545

    feat: create portal template

--- src/templates/PortalTemplate.tsx
+++ src/templates/PortalTemplate.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import { PropsWithChildren } from "react";
 
-function PortalTemplate() {
+function PortalTemplate({ children }: PropsWithChildren) {
+  const backdropNode = document.getElementById("backdrop-root");
   return (
-    <div>PortalTemplate</div>
-  )
+    <div>
+      {backdropNode ? (
+        <div className="w-screen h-screen bg-black opacity-70 z-50 left-0 right-0 fixed">
+          <div className="relative">{children}</div>
+        </div>
+      ) : null}
+    </div>
+  );
 }
 
-export default PortalTemplate
+export default PortalTemplate;
