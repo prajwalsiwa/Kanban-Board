b2fc1f6ee811dd92f35e39a4f96e08b0bd612b7a
prajwalsiwa <prajwal.siwa1@gmail.com>
2024-08-06 09:00:48 +0545

    fix: edit style

--- src/views/Dashboard/index.tsx
+++ src/views/Dashboard/index.tsx
@@ -4,16 +4,19 @@ import SideBar from "@Components/common/SideBar";
 
 export default function Dashboard() {
   return (
-    <div className="flex w-full h-full">
-      <div className="flex justify-start">
-        <SideBar />
-      </div>
-      <div className="w-full h-full flex flex-col">
-        <Navbar />
-        <div className="w-full h-full">
-          <Workspace />
+    <>
+      
+      <div className="flex ">
+        <div className="flex justify-start">
+          <SideBar />
+        </div>
+        <div className="w-full h-full flex flex-col">
+          <Navbar />
+          <div className="w-full h-full">
+            <Workspace />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
