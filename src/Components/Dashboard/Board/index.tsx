ff02cf869c8acf81bb4d4245938bb79fd3ed0cbf
prajwalsiwa <prajwal.siwa1@gmail.com>
2024-08-06 08:59:38 +0545

    feat: create board component

---
+++ src/Components/Dashboard/Board/index.tsx
@@ -0,0 +1,65 @@
+import Button from "@Components/common/Button";
+import RoundedContainer from "@Components/common/RoundedContainer";
+import { ChangeEvent, useState } from "react";
+import { message } from "../WorkSpace";
+
+interface boardProps {
+  taskList: message[];
+  label: string;
+  labelColor?: string;
+}
+
+function Board({ taskList, label, labelColor }: boardProps) {
+  const [value, setValue] = useState<string | null>(null);
+  const [newMessageList, setnewMessageList] = useState(taskList);
+
+  const handleAddnewMessageList = () => {
+    if (value) {
+      setnewMessageList((msg) => [
+        ...msg,
+        {
+          id: Math.random(),
+          message: value,
+        },
+      ]);
+      setValue(null);
+    }
+  };
+
+  function handleChange(event: ChangeEvent<HTMLTextAreaElement>) {
+    setValue(event.target.value);
+  }
+
+  return (
+    <div className="w-[18rem] h-fit  border border-gray-600 rounded-lg">
+      <div className="flex justify-start pl-4 py-2 border-b">
+        <p className={`title text-lg font-semibold ${labelColor}`}>{label}</p>
+      </div>
+      <div className="p-2 flex flex-col gap-2">
+        {newMessageList.map((msg, index) => (
+          <div key={index}>
+            <RoundedContainer message={msg.message} />
+          </div>
+        ))}
+        <div className="w-full">
+          <textarea
+            className="bg-transparent border border-gray-500 outline-none rounded-lg min-h-11 p-1 w-full"
+            onChange={handleChange}
+            value={value || ""}
+            placeholder="Enter here"
+          />
+          <div>
+            <Button
+              className="border border-gray-500 rounded-md py-2 hover:bg-slate-500 hover:text-black px-2"
+              leftIconName="add"
+              label="Add item"
+              onClick={handleAddnewMessageList}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Board;
