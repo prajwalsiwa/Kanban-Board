8c88b3af14aade3e946593256a0ca7efa5bdc7c5
prajwalsiwa <prajwal.siwa1@gmail.com>
2024-08-06 08:56:58 +0545

    refactor: edit protal template

--- src/templates/PortalTemplate.tsx
+++ src/templates/PortalTemplate.tsx
@@ -1,14 +1,18 @@
 import { PropsWithChildren } from "react";
+import { createPortal } from "react-dom";
 
 function PortalTemplate({ children }: PropsWithChildren) {
   const backdropNode = document.getElementById("backdrop-root");
   return (
     <div>
-      {backdropNode ? (
-        <div className="w-screen h-screen bg-black opacity-70 z-50 left-0 right-0 fixed">
-          <div className="relative">{children}</div>
-        </div>
-      ) : null}
+      {backdropNode
+        ? createPortal(
+            <div className="w-screen h-screen bg-black opacity-70 z-50 left-0 right-0 fixed">
+              {children}
+            </div>,
+            backdropNode
+          )
+        : null}
     </div>
   );
 }
