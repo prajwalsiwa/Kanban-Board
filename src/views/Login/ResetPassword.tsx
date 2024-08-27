4e5ed6aaff5b990bed95ca8808444b35745f8cfa
prajwalsiwa <prajwal.siwa1@gmail.com>
2024-08-27 10:58:18 +0545

    feat: make `resetPassword` component responsive

--- src/views/Login/ResetPassword.tsx
+++ src/views/Login/ResetPassword.tsx
@@ -12,11 +12,11 @@ function ResetPassword() {
       <div className="image h-full w-[60%] ">
         <img src={kanban} alt="" className="w-full h-full object-cover" />
       </div>
-      <div className="login h-full  w-[40%]  bg-gray-100 flex gap-36  pt-20 flex-col">
+      <div className="login h-full  w-[55rem]  bg-gray-100 flex items-center justify-center  gap-10   flex-col">
         <div className="flex flex-col w-full justify-center items-center">
           <h1>Kanban Board</h1>
         </div>
-        <div className="flex px-56 gap-12 flex-col w-full">
+        <div className="flex px-56 gap-12 flex-col justify-center items-center w-full">
           <h2 className="text-xl">Reset Password</h2>
           <div className="flex flex-col gap-6">
             <div className="flex flex-col justify-start items-start w-full relative ">
@@ -24,7 +24,7 @@ function ResetPassword() {
               <input
                 id="password"
                 type={isVisible ? "text" : "password"}
-                className="border  rounded-lg border-b-1 bg-white naxatw-border-gray-300 p-1 py-2 w-full focus:!outline focus:outline-1 focus:outline-gray-500 outline-gray-300"
+                className="border  rounded-lg border-b-1 bg-white naxatw-border-gray-300 p-1 py-2 w-[20rem] focus:!outline focus:outline-1 focus:outline-gray-500 outline-gray-300"
               />
               <i
                 className="material-symbols-outlined absolute right-3 bottom-2 cursor-pointer font-normal"
@@ -39,7 +39,7 @@ function ResetPassword() {
               <input
                 id="password"
                 type={isVisible ? "text" : "password"}
-                className="border  rounded-lg border-b-1 bg-white naxatw-border-gray-300 p-1 py-2 w-full focus:!outline focus:outline-1 focus:outline-gray-500 outline-gray-300"
+                className="border  rounded-lg border-b-1 bg-white naxatw-border-gray-300 p-1 py-2 w-[20rem] focus:!outline focus:outline-1 focus:outline-gray-500 outline-gray-300"
               />
               <i
                 className="material-symbols-outlined absolute right-3 bottom-2 cursor-pointer font-normal"
@@ -50,7 +50,7 @@ function ResetPassword() {
               {/* <i className="material-symbols-outlined absolute right-3 bottom-2 cursor-pointer font-normal">visibility_off</i> */}
             </div>
 
-            <div className="button-section  gap-6  flex flex-col w-full py-6">
+            <div className="button-section  gap-6  flex flex-col w-[20rem] py-6">
               <button
                 type="submit"
                 className="bg-blue-700 rounded-lg text-white hover:bg-blue-500 w-full py-2 "
