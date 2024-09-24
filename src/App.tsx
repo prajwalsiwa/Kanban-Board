6db3dff43689002bde5e9f420121d6f73ff61946
prajwalsiwa <prajwal.siwa1@gmail.com>
2024-09-24 23:11:06 +0545

    feat: include dom to code

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
+import { initDomToCode } from "dom-to-code";
 import "./App.css";
 import appRoutes from "./routes/appRoutes";
 import generateRoutes from "./routes/generateRoutes";
+import { useEffect } from "react";
+import { ENV_MODE } from "@global";
 
 function App() {
+  useEffect(() => {
+    if (ENV_MODE !== "production") {
+      initDomToCode();
+    }
+  }, []);
+
   return (
-    <div className="w-full h-full">
-      {generateRoutes({ routes: appRoutes })}
-    </div>
+    <div className="w-full h-full">{generateRoutes({ routes: appRoutes })}</div>
   );
 }
 
